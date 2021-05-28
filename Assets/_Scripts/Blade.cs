1748f6c9d298e6cd13d405500b2f99657be6ab63
NavarroDGerardo <dgnvrrg@gmail.com>
2021-05-28 16:52:15 -0500

    first commit

---
+++ Assets/_Scripts/Blade.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    float angleZ = 0;
+    float dz = 0.3f;
+    Vector3[] orgiinalPoints;
+    MeshFilter mf;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
