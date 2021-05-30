bfc1110ae68f7fe26746353d0bf5829f853a9735
ed9ar <a01339939@itesm.mx>
2021-05-29 22:45:20 -0500

    Rain
    Rain

---
+++ Assets/_Scripts/Emitter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Emitter : MonoBehaviour
+{
+    public GameObject ParticlePrefab;
+    public int numParticles;
+    private GameObject[] particles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles = new GameObject[numParticles];
+
+        for (int p = 0; p < numParticles; p++)
+        {
+            GameObject part = Instantiate(ParticlePrefab);
+            //Each particle will have a random radius in the range[0.3, 0.9].
+            float diam = Random.Range(0.05f, 0.06f); 
+            part.transform.localScale = new Vector3(diam, diam, diam);
+            //Each particle will have a random material color.
+            //The red diffuse channel in the range[0.2, 0.7](thefull red color will be reservedfor collisions). The green and blue diffuse channelsin the range[0.2, 1.0].
+            Color c = new Color(0, 0, Random.Range(0.2f, 1f));
+            Renderer rend = part.GetComponent<Renderer>();
+            rend.material.SetColor("_Color", c);
+            particles[p] = part;
+
+            // Get a reference to the Particle.cs script inside the prefab:
+            Particle pScript = part.GetComponent<Particle>();
+            pScript.color = new Vector3(c.r, c.g, c.b);
+            pScript.r = diam / 2f;
+            pScript.g = 9.81f;
+            pScript.currPos = new Vector3(Random.Range(1f, 10f), Random.Range(6f, 12f), Random.Range(-2f, 2f));
+            pScript.iniPos = new Vector3(Random.Range(1f, 10f), Random.Range(6f, 16f), Random.Range(-1f, 1f));;
+            pScript.prevPos = pScript.currPos;
+            //Each particle’s mass will equal the radius times 2.
+            pScript.m = pScript.r * 2;
+            pScript.restitution = 0.7f;
+            pScript.f = Vector3.zero;
+
+            
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //When two particles collide, they will change theirdiffuse color tored.
+        
+    }
+}
---
+++ Assets/_Scripts/Particle.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Particle : MonoBehaviour
+{
+    public float r;
+    public float g;
+    public float m;
+    public float lastfx, lastfz, lastfy;
+    public Vector3 f;
+    public Vector3 a;
+    public Vector3 iniPos;
+    public Vector3 prevPos;
+    public Vector3 currPos;
+    public Vector3 airForce;
+    public float restitution;
+    public Vector3 color;
+    public Vector3 vel;
+    public int rain = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //The particles will explode from the emitter at(0,0, 0), with random forces in ±X ±Y and ±Z
+        
+    }
+ 
+ 
+    void CheckFloor()
+    {
+        
+        if (currPos.y < r)
+        {
+            //Debug.Log("Floor");
+           
+            currPos = iniPos;
+            prevPos = iniPos;
+            
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(currPos);
+        if(rain == 1){
+            if(Mathf.Abs(currPos.y - prevPos.y) < 0.00001f && Mathf.Abs(currPos.y - r) < 0.00001f)
+            {
+                currPos.y = r;
+                prevPos.y = r;
+            
+            }
+            else
+            {
+                
+                f.y = -m * g * 0.2f;
+                f.x = -m * g *  0.3f;
+                
+            
+                if(currPos.y != prevPos.y)
+                {
+                    vel = (currPos - prevPos) / Time.deltaTime;
+                    
+                    f.y = f.y + r * 0.001f * vel.magnitude;
+            
+                }
+            }
+
+            a = f / m;
+            Vector3 temp = currPos;
+            float dt = Time.deltaTime;
+            currPos = 2 * currPos - prevPos + a * dt * dt;
+            prevPos = temp;
+            CheckFloor();
+            transform.localPosition = currPos;
+        }
+        
+        
+
+        if (Input.GetKeyDown("r"))
+        {
+            rain = -rain;
+        }
+    }
+
+
+   
+
+}
